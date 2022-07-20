0f03622468d051d028c809cc6345ae37ae475794
Bere <bre_kc073@hotmail.com>
2022-07-20 11:20:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import './App.css';
-import Button from './Components/Button';
+import './App.css'
+import Button from './Components/Button'
+import users from './Assets/users'
 
 function App() {
+  const buttons = [
+    {
+      text: 'Botón 1',
+      color: 'primary'
+    },
+    {
+      text: 'Botón 2',
+      color: 'secondary'
+    },
+    {
+      text: 'Botón 3',
+      color: 'success'
+    },
+    {
+      text: 'Botón 4',
+      color: 'danger'
+    },
+    {
+      text: 'Botón 5',
+      color: 'warning'
+    }
+  ]
   return (
     <div className='App'>
       <div className='d-flex justify-content-between w-50 mt-3 mx-auto'>
-        <Button text='Atras' color='danger'></Button>
-        <Button text='Siguiente' color='success'></Button>
-        <Button></Button>
+        {buttons
+          .filter(button => button.color !== 'primary')
+          .map((button, index) => {
+            <Card style = {{width: '18rem'}}>
+              <img alt="Card image" src="https://picsum.photos/300/200"/>
+              <CardBody>
+                <CardTitle tag="h5"> Card title </CardTitle>
+                <CardSubtitle className="mb-2 text-muted" tag="h6"> Card subtitle </CardSubtitle>
+                <CardText> Some quick example text to build on the card title and make up the bulk of the card's content. </CardText>
+              </CardBody>
+            </Card >
+          })}
       </div>
     </div>
-    )
+  )
+
+
+
 }
 
-export default App;
+
+export default App
---
+++ src/Components/List/list.js
@@ -0,0 +1,14 @@
+import { Card, Cardbody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap'
+
+const CardCustom = () =>{
+    const {} = user
+}
+
+  <Card style = {{width: '18rem'}}>
+    <img alt="Card image" src="https://picsum.photos/300/200"/>
+    <CardBody>
+        <CardTitle tag="h5"> Card title </CardTitle>
+        <CardSubtitle className="mb-2 text-muted" tag="h6"> Card subtitle </CardSubtitle>
+        <CardText> Some quick example text to build on the card title and make up the bulk of the card's content. </CardText>
+    </CardBody>
+  </Card >
