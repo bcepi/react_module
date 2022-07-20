7fa7a5319f2c53eb4ac0f984079eb6e4d35cbac7
Bere <bre_kc073@hotmail.com>
2022-07-19 20:34:13 -0500

    add components folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='d-flex justify-content-between w-50 mt-3 mx-auto'>
+        <Button text='Atras' color='danger'></Button>
+        <Button text='Siguiente' color='success'></Button>
+        <Button></Button>
+      </div>
     </div>
-  );
+    )
 }
 
 export default App;
