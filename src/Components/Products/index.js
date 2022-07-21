ace7f73390bbb899c3f5ce54e14000e554c47c7f
Bere <bre_kc073@hotmail.com>
2022-07-21 18:33:13 -0500

    add textt color in card rate info by rate products clasification

--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -3,7 +3,25 @@ import {Card, CardTitle, CardBody, CardSubtitle, CardText} from "reactstrap"
 const ProductCard = props => {
     console.log(props)
     const {title, description, category, image, rating } = props.cardData
-        
+    
+    const color = (rating) =>{
+        let ratingColor= ''
+
+        if(rating<3){
+            ratingColor = 'text-danger'
+        } else if(3<rating<4.5){
+            ratingColor= 'text-warning'
+        } else if (rating> 4.5){
+            ratingColor ='text-success'
+        } else{
+            ratingColor='text-muted'
+        }
+
+        return ratingColor
+    }   
+    
+    const textColor = color(rating.rate)
+
     return (
     <div className="col">
         <Card className="d-flex flex-row CardCustom"> 
@@ -15,7 +33,7 @@ const ProductCard = props => {
                     <CardTitle tag="h5">{title}</CardTitle>
                     <CardSubtitle className="mb-2 text-muted" tag="h6">{category}</CardSubtitle>
                     <CardText className="d-block text">{description.substring(0,10)}</CardText>
-                    <CardSubtitle className="mb-2 text-muted" tag="h6">{rating.rate}</CardSubtitle>
+                    <CardSubtitle className="mb-2" tag="h6"><p className={`${textColor}`}>{rating.rate}</p></CardSubtitle>
                 </CardBody>
             </div>
             
